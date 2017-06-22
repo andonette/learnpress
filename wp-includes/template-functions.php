5f88a508b7107a1758d356bdafee6dcf644d31b2
andonette <andi@madebyfactory.com>
2017-06-22 19:58:23 +0100

    template functions file

---
+++ wp-includes/template-functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* new and improved ! now with more querystring stuff ! */
+
+if (!isset($querystring_start)) {
+	$querystring_start = '?';
+	$querystring_equal = '=';
+	$querystring_separator = '&amp;';
+}
+
+?>
