5705500e6f1070c2bab21a56074ada36a9ea635a
andonette <andi@madebyfactory.com>
2017-06-22 20:19:46 +0100

    inc template functions

--- xmlrpc.php
+++ xmlrpc.php
@@ -7,4 +7,5 @@ include('wp-config.php');
 
 require_once(ABSPATH.WPINC."/class-xmlrpc.php");
 require_once(ABSPATH.WPINC."/class-xmlrpcs.php");
+require_once(ABSPATH.WPINC."/template-functions.php");
 ?>
