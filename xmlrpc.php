f233b0371a2fa029d0a418db40829fb12bce10aa
andonette <andi@madebyfactory.com>
2017-06-22 20:17:35 +0100

    xmlrpc

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,3 +1,9 @@
 <?php
 
+# fix for mozBlog and other cases where '<?xml' isn't on the very first line
+$HTTP_RAW_POST_DATA = trim($HTTP_RAW_POST_DATA);
+
+include('wp-config.php');
+
+require_once(ABSPATH.WPINC."/class-xmlrpc.php");
 ?>
