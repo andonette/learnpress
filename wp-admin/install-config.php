931ddfe271f273edbe0d2434d9d9856222ba0391
andonette <andi@madebyfactory.com>
2017-06-24 05:01:39 +0100

    missing admin files & formatting changes

--- wp-admin/install-config.php
+++ wp-admin/install-config.php
@@ -12,31 +12,31 @@ if (!$step) $step = 0;
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <style media="screen" type="text/css">
     <!--
-  body {
-    font-family: Georgia, "Times New Roman", Times, serif;
-    margin-left: 15%;
-    margin-right: 15%;
-  }
-  #logo {
-    margin: 0;
-    padding: 0;
-    background-image: url(http://wordpress.org/images/logo.png);
-    background-repeat: no-repeat;
-    height: 60px;
-    border-bottom: 4px solid #333;
-  }
-  #logo a {
-    display: block;
-    height: 60px;
-  }
-  #logo a span {
-    display: none;
-  }
-  p, li {
-    line-height: 140%;
-  }
+	body {
+		font-family: Georgia, "Times New Roman", Times, serif;
+		margin-left: 15%;
+		margin-right: 15%;
+	}
+	#logo {
+		margin: 0;
+		padding: 0;
+		background-image: url(http://wordpress.org/images/logo.png);
+		background-repeat: no-repeat;
+		height: 60px;
+		border-bottom: 4px solid #333;
+	}
+	#logo a {
+		display: block;
+		height: 60px;
+	}
+	#logo a span {
+		display: none;
+	}
+	p, li {
+		line-height: 140%;
+	}
     -->
-  </style>
+	</style>
 </head>
 <body> 
 <h1 id="logo"><a href="http://wordpress.org"><span>WordPress</span></a></h1> 
@@ -44,7 +44,7 @@ if (!$step) $step = 0;
 
 switch($step) {
 
-  case 0:
+	case 0:
 ?> 
 <p>Welcome to WordPress. Before getting started, we need some information on the database. You will need to know the following items before proceeding.</p> 
 <ol> 
@@ -56,10 +56,10 @@ switch($step) {
 </ol> 
 <p>In all likelyhood, these items were supplied to you by your ISP. If you do not have this information, then you will need to contact them before you can continue. If you&#8217;re all ready, <a href="install-config.php?step=1">let&#8217;s go</a>! </p> 
 <?php
-  break;
+	break;
 
-  case 1:
-  ?> 
+	case 1:
+	?> 
 </p> 
 <form method="post" action="install-config.php?step=2"> 
   <p>Below you should enter your database connection details. If you're not sure about these, contact your host. </p>
@@ -93,21 +93,21 @@ switch($step) {
   <input name="submit" type="submit" value="Submit" /> 
 </form> 
 <?php
-  break;
-  
-  case 2:
-  $dbname = $HTTP_POST_VARS['dbname'];
+	break;
+	
+	case 2:
+	$dbname = $HTTP_POST_VARS['dbname'];
     $uname = $HTTP_POST_VARS['uname'];
     $passwrd = $HTTP_POST_VARS['pwd'];
     $dbhost = $HTTP_POST_VARS['dbhost'];
-  $prefix = $HTTP_POST_VARS['prefix'];
-  if (empty($prefix)) $prefix = 'wp_';
+	$prefix = $HTTP_POST_VARS['prefix'];
+	if (empty($prefix)) $prefix = 'wp_';
     
-  if (!file_exists('../wp-config-sample.php'))
-    die('Sorry, I need a wp-config-sample.php file to work from. Please re-upload this file from your WordPress installation.');
+	if (!file_exists('../wp-config-sample.php'))
+		die('Sorry, I need a wp-config-sample.php file to work from. Please re-upload this file from your WordPress installation.');
     $configFile = file('../wp-config-sample.php');
     
-  if (!is_writable('../')) die("Sorry, I can't write to the directory. You'll have to either change the permissions on your WordPress directory or create your wp-config.php manually.");
+	if (!is_writable('../')) die("Sorry, I can't write to the directory. You'll have to either change the permissions on your WordPress directory or create your wp-config.php manually.");
     $handle = fopen('../wp-config.php', 'w');
 
     foreach ($configFile as $line_num => $line) {
@@ -124,19 +124,19 @@ switch($step) {
             case "define('DB_HOST'":
                 fwrite($handle, str_replace("localhost", $dbhost, $line));
                 break;
-      case '$table_prefix  =':
-        fwrite($handle, str_replace('wp_', $prefix, $line));
-        break;
+			case '$table_prefix  =':
+				fwrite($handle, str_replace('wp_', $prefix, $line));
+				break;
             default:
                 fwrite($handle, $line);
         }
     }
     fclose($handle);
-  chmod('../wp-config.php', 0666);
+	chmod('../wp-config.php', 0666);
 ?> 
 <p>All right sparky! You've made it through this part of the installation. WordPress can now communicate with your database. If you are ready, time now to <a href="install.php">run the install!</a></p> 
 <?php
-  break;
+	break;
 
 }
 ?> 
--- wp-admin/options-permalink.php
+++ wp-admin/options-permalink.php
@@ -3,56 +3,56 @@ $title = 'Permalink Options';
 $this_file = 'options.php';
 
 function add_magic_quotes($array) {
-  foreach ($array as $k => $v) {
-    if (is_array($v)) {
-      $array[$k] = add_magic_quotes($v);
-    } else {
-      $array[$k] = addslashes($v);
-    }
-  }
-  return $array;
+	foreach ($array as $k => $v) {
+		if (is_array($v)) {
+			$array[$k] = add_magic_quotes($v);
+		} else {
+			$array[$k] = addslashes($v);
+		}
+	}
+	return $array;
 }
 
 if (!get_magic_quotes_gpc()) {
-  $HTTP_GET_VARS    = add_magic_quotes($HTTP_GET_VARS);
-  $HTTP_POST_VARS   = add_magic_quotes($HTTP_POST_VARS);
-  $HTTP_COOKIE_VARS = add_magic_quotes($HTTP_COOKIE_VARS);
+	$HTTP_GET_VARS    = add_magic_quotes($HTTP_GET_VARS);
+	$HTTP_POST_VARS   = add_magic_quotes($HTTP_POST_VARS);
+	$HTTP_COOKIE_VARS = add_magic_quotes($HTTP_COOKIE_VARS);
 }
 
 $wpvarstoreset = array('action','standalone', 'option_group_id');
 for ($i=0; $i<count($wpvarstoreset); $i += 1) {
-  $wpvar = $wpvarstoreset[$i];
-  if (!isset($$wpvar)) {
-    if (empty($HTTP_POST_VARS["$wpvar"])) {
-      if (empty($HTTP_GET_VARS["$wpvar"])) {
-        $$wpvar = '';
-      } else {
-        $$wpvar = $HTTP_GET_VARS["$wpvar"];
-      }
-    } else {
-      $$wpvar = $HTTP_POST_VARS["$wpvar"];
-    }
-  }
+	$wpvar = $wpvarstoreset[$i];
+	if (!isset($$wpvar)) {
+		if (empty($HTTP_POST_VARS["$wpvar"])) {
+			if (empty($HTTP_GET_VARS["$wpvar"])) {
+				$$wpvar = '';
+			} else {
+				$$wpvar = $HTTP_GET_VARS["$wpvar"];
+			}
+		} else {
+			$$wpvar = $HTTP_POST_VARS["$wpvar"];
+		}
+	}
 }
 
 require_once('optionhandler.php');
 
 if ($HTTP_POST_VARS['Submit'] == 'Update') {
-  update_option('permalink_structure', $HTTP_POST_VARS['permalink_structure']);
-  $permalink_structure = $HTTP_POST_VARS['permalink_structure'];
+	update_option('permalink_structure', $HTTP_POST_VARS['permalink_structure']);
+	$permalink_structure = $HTTP_POST_VARS['permalink_structure'];
 } else {
-  $permalink_structure = get_settings('permalink_structure');
+	$permalink_structure = get_settings('permalink_structure');
 }
 
 
 switch($action) {
 
 default:
-  $standalone = 0;
-  include_once('admin-header.php');
-  if ($user_level <= 3) {
-    die("You have no right to edit the options for this blog.<br>Ask for a promotion to your <a href=\"mailto:$admin_email\">blog admin</a> :)");
-  }
+	$standalone = 0;
+	include_once('admin-header.php');
+	if ($user_level <= 3) {
+		die("You have no right to edit the options for this blog.<br>Ask for a promotion to your <a href=\"mailto:$admin_email\">blog admin</a> :)");
+	}
 ?>
  <ul id="adminmenu2"> 
   <?php
@@ -101,25 +101,25 @@ $site_root = preg_replace('|([^/]*)(.*)|i', '$2', $site_root);
 if ('/' != substr($site_root, -1)) $site_root = $site_root . '/';
 
 $rewritecode = array(
-  '%year%',
-  '%monthnum%',
-  '%day%',
-  '%postname%',
-  '%post_id%'
+	'%year%',
+	'%monthnum%',
+	'%day%',
+	'%postname%',
+	'%post_id%'
 );
 $rewritereplace = array(
-  '([0-9]{4})?',
-  '([0-9]{1,2})?',
-  '([0-9]{1,2})?',
-  '([0-9a-z-]+)?',
-  '([0-9]+)?'
+	'([0-9]{4})?',
+	'([0-9]{1,2})?',
+	'([0-9]{1,2})?',
+	'([0-9a-z-]+)?',
+	'([0-9]+)?'
 );
 $queryreplace = array (
-  'year=',
-  'monthnum=',
-  'day=',
-  'name=',
-  'p='
+	'year=',
+	'monthnum=',
+	'day=',
+	'name=',
+	'p='
 );
 
 
@@ -133,8 +133,8 @@ preg_match_all('/%.+?%/', $permalink_structure, $tokens);
 
 $query = 'index.php?';
 for ($i = 0; $i < count($tokens[0]); ++$i) {
-  if (0 < $i) $query .= '&';
-  $query .= str_replace($rewritecode, $queryreplace, $tokens[0][$i]) . '$'. ($i + 1);
+	if (0 < $i) $query .= '&';
+	$query .= str_replace($rewritecode, $queryreplace, $tokens[0][$i]) . '$'. ($i + 1);
 }
 ++$i;
 // Add post paged stuff
@@ -143,8 +143,8 @@ $query .= "&page=$$i";
 
 // Code for nice categories, currently not very flexible
 $front = substr($permalink_structure, 0, strpos($permalink_structure, '%'));
-    $catmatch = $front . 'category/';
-    $catmatch = preg_replace('|^/+|', '', $catmatch);
+		$catmatch = $front . 'category/';
+		$catmatch = preg_replace('|^/+|', '', $catmatch);
 
 ?> 
 <form action"">
@@ -152,7 +152,7 @@ $front = substr($permalink_structure, 0, strpos($permalink_structure, '%'));
 RewriteBase <?php echo $site_root; ?> 
 RewriteRule ^<?php echo $match; echo '$ ' . $site_root . $query ?> [QSA]
 RewriteRule ^<?php echo $catmatch; ?>(.*) <?php echo $site_root; ?>index.php?category_name=$1 [QSA]</textarea> 
-  </form>
+	</form>
 </div> 
 <?php
 } else {
